26a05871d8cf93ef45bcf99ba230c4115ea7d84a
LeshaRB <lesharb@gmail.com>
2015-04-15 10:45:19 +0200

    First commit

---
+++ GWTTestClient/src/main/java/by/gwttest/client/client/application/home/HomeModule.java
@@ -0,0 +1,11 @@
+package by.gwttest.client.client.application.home;
+
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+
+public class HomeModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        bindPresenter(HomePagePresenter.class, HomePagePresenter.MyView.class, HomePageView.class,
+                HomePagePresenter.MyProxy.class);
+    }
+}
